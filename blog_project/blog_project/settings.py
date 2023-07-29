2ded5a9096e90ab32db06c502ba4a09ecd0c8971
Sergey <svg95@lsit.ru>
2023-07-29 14:47:49 +0300

    Chapter 2

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -118,3 +118,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# email configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'djsedoy95@gmail.com'
+EMAIL_HOST_PASSWORD = 'eiambmjelwiupsrl'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
